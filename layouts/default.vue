21b4b1239eb2b4600402eb18768c422dc5481d44
somchai3310 <somchaibefore@gmail.com>
2024-03-13 15:48:18 +0700

    update

--- layouts/default.vue
+++ layouts/default.vue
@@ -56,7 +56,7 @@
     </header>
 
     <!-- content -->
-    <div class="container mx-auto p-4">
+    <div class="container mx-auto px-[70px]">
       <slot />
     </div>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,19 @@
 <template>
   <div>
     <!-- Banner side -->
-    <div class="bannerSide">
+    <div class="bannerSide mx-38">
       <div class="carousel w-full">
         <div id="item1" class="carousel-item w-full">
-          <img src="https://daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.jpg" class="w-full" />
+          <img src="~/assets/images/slide/slide1.png" class="w-full" />
         </div>
         <div id="item2" class="carousel-item w-full">
-          <img src="https://daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg" class="w-full" />
+          <img src="~/assets/images/slide/slide2.png" class="w-full" />
         </div>
         <div id="item3" class="carousel-item w-full">
-          <img src="https://daisyui.com/images/stock/photo-1414694762283-acccc27bca85.jpg" class="w-full" />
+          <img src="~/assets/images/slide/slide3.png" class="w-full" />
         </div>
         <div id="item4" class="carousel-item w-full">
-          <img src="https://daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.jpg" class="w-full" />
+          <img src="~/assets/images/slide/slide4.png" class="w-full" />
         </div>
       </div>
       <div class="flex justify-center w-full py-2 gap-2">
@@ -26,19 +26,101 @@
     </div>
     <!-- notebook / gaming -->
     <div class="notebookGaming">
-      <h1>notebookGaming</h1>
+      <div class="top">
+        <!-- <div role="tablist" class="tabs tabs-bordered"> -->
+        <div role="tablist" class="tabs tabs-bordered">
+          <input type="radio" name="my_tabs_1" role="tab" class="tab" aria-label="Notebook" checked />
+          <div role="tabpanel" class="tab-content py-10">
+            <div class="flex flex-wrap gap-2">
+              <div class="bg-red-500 w-[200px] h-[200px]"></div>
+              <div class="bg-red-500 w-[200px] h-[200px]"></div>
+              <div class="bg-red-500 w-[200px] h-[200px]"></div>
+              <div class="bg-red-500 w-[200px] h-[200px]"></div>
+              <div class="bg-red-500 w-[200px] h-[200px]"></div>
+              <div class="bg-red-500 w-[200px] h-[200px]"></div>
+              <div class="bg-red-500 w-[200px] h-[200px]"></div>
+              <div class="bg-red-500 w-[200px] h-[200px]"></div>
+              <div class="bg-red-500 w-[200px] h-[200px]"></div>
+            </div>
+          </div>
+
+          <input type="radio" name="my_tabs_1" role="tab" class="tab" aria-label="GAMING GEAR" />
+          <div role="tabpanel" class="tab-content py-10">
+            <div class="flex flex-wrap gap-2">
+              <div class="bg-blue-500 w-[200px] h-[200px]"></div>
+              <div class="bg-blue-500 w-[200px] h-[200px]"></div>
+              <div class="bg-blue-500 w-[200px] h-[200px]"></div>
+              <div class="bg-blue-500 w-[200px] h-[200px]"></div>
+              <div class="bg-blue-500 w-[200px] h-[200px]"></div>
+              <div class="bg-blue-500 w-[200px] h-[200px]"></div>
+              <div class="bg-blue-500 w-[200px] h-[200px]"></div>
+              <div class="bg-blue-500 w-[200px] h-[200px]"></div>
+              <div class="bg-blue-500 w-[200px] h-[200px]"></div>
+            </div>
+          </div>
+          <button class="btn btn-primary flex justify-end">ทั้งหมด</button>
+        </div>
+      </div>
     </div>
     <!-- IT -->
     <div class="it">
-      <h1>it</h1>
+      <div class="top">
+        <div class="text">อุปกรณ์ไอที</div>
+        <div class="right">
+          <button class="btn btn-primary flex justify-end">ทั้งหมด</button>
+          <!-- ปุ่ม ซ้าย ขวา -->
+        </div>
+      </div>
+      <div class="it">
+        <div class="one-card">
+          <img class="w-[239px] h-[239] rounded" src="~/assets/images/it/custom-radio-img-1.png" alt="">
+          <p>treasure heroes</p>
+        </div>
+      </div>
     </div>
     <!-- side -->
     <div class="side">
-      <h1>side</h1>
+      <div class="carousel carousel-center  p-4 space-x-4 bg-neutral rounded-box">
+        <div class="carousel-item">
+          <img src="https://daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg" class="rounded-box" />
+        </div>
+        <div class="carousel-item">
+          <img src="https://daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg" class="rounded-box" />
+        </div>
+        <div class="carousel-item">
+          <img src="https://daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg" class="rounded-box" />
+        </div>
+        <div class="carousel-item">
+          <img src="https://daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg" class="rounded-box" />
+        </div>
+        <div class="carousel-item">
+          <img src="https://daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg" class="rounded-box" />
+        </div>
+        <div class="carousel-item">
+          <img src="https://daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg" class="rounded-box" />
+        </div>
+        <div class="carousel-item">
+          <img src="https://daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg" class="rounded-box" />
+        </div>
+      </div>
+      <div class="flex justify-center w-full py-2 gap-2">
+        <a href="#item1" class="btn btn-xs">1</a>
+        <a href="#item2" class="btn btn-xs">2</a>
+        <a href="#item3" class="btn btn-xs">3</a>
+        <a href="#item4" class="btn btn-xs">4</a>
+      </div>
     </div>
     <!-- อุปกรณ์เสริม -->
     <div class="accessories">
-      <h1>accessories</h1>
+      <div class="top">
+        <div class="left">
+          icons
+          <p>อุปกรณ์เสริม</p>
+        </div>
+        <div class="right">
+          <button class="btn btn-primary flex justify-end">ทั้งหมด</button>
+        </div>
+      </div>
     </div>
     <!-- provider Google -->
     <div class="provider">
