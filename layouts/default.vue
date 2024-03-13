d2d1dc0bfa7928b14c63b69569da60f260319ec2
somchai3310 <somchaibefore@gmail.com>
2024-03-13 12:45:13 +0700

    วางโครงสร้าง

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,58 @@
 <template>
   <div>
-    <header class="shadow-sm bg-white">
-      <nav class="container mx-auto p-1 flex justiy-between">
+    <header class="shadow-sm bg-white flex justify-center">
+      <div class="authLoggedIn container mx-auto p-1 flex justiy-between">
+        <div class="left-header flex flex-auto">
+          <div class="logo">
+            <NuxtLink to="/" class="">
+              <img src="~/assets/images/logo/logo.png" alt="menu-icon" class="w-20" />
+            </NuxtLink>
+          </div>
+          <div class="page flex">
+            <div class="flex h-11">
+              <nuxt-icon name="home" color="black"></nuxt-icon>
+              <div>หน้าหลัก</div>
+            </div>
+            <div class="flex h-11">
+              <nuxt-icon name="faq"></nuxt-icon>
+              <div>faq</div>
+            </div>
+            <div class="flex h-11">
+              <nuxt-icon name="blog"></nuxt-icon>
+              <div>บทความ</div>
+            </div>
+            <div class="flex h-11">
+              <nuxt-icon name="contact"></nuxt-icon>
+              <div>ติดต่อเรา</div>
+            </div>
+          </div>
+        </div>
+        <div class="right-header flex justify-end">
+          <div class="register-login">
+            <button class="btn btn-primary w-18">Register</button>
+            <button class="btn btn-secondary w-18 border-none">Login</button>
+          </div>
+          <div class="lang flex bg-gray-500">
+            <div class="langText">
+              <p>TH</p>
+            </div>
+            <div class="flag">
+              <img height="14" src="~/assets/images/flag/th.png" alt="flag">
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- <nav class="container mx-auto p-1 flex justiy-between">
 
-        <NuxtLink to="/" class="">
-          <img src="~/assets/images/logo/logo.png" alt="menu-icon" class="w-20" />
-        </NuxtLink>
+
         <ul class="flex gap-4">
-          <li><inline-svg src="~/assets/icons/home.svg" width="20" height="20" aria-label="see-all-icon" class="ml-1" />หน้าหลัก</li>
+          <li><inline-svg src="~/assets/icons/home.svg" width="20" height="20" aria-label="see-all-icon"
+              class="ml-1" />หน้าหลัก</li>
           <li>faq</li>
           <li>บทความ</li>
           <li>ติดต่อเรา</li>
         </ul>
-      </nav>
+      </nav> -->
     </header>
 
     <!-- content -->
@@ -34,4 +74,8 @@
 
 </script>
 
-<style scoped></style>
+<style scoped>
+.nuxt-icon svg{
+  color: black;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,54 @@
 <template>
-    <div>
-        Home page
+  <div>
+    <!-- Banner side -->
+    <div class="bannerSide">
+      <div class="carousel w-full">
+        <div id="item1" class="carousel-item w-full">
+          <img src="https://daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.jpg" class="w-full" />
+        </div>
+        <div id="item2" class="carousel-item w-full">
+          <img src="https://daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg" class="w-full" />
+        </div>
+        <div id="item3" class="carousel-item w-full">
+          <img src="https://daisyui.com/images/stock/photo-1414694762283-acccc27bca85.jpg" class="w-full" />
+        </div>
+        <div id="item4" class="carousel-item w-full">
+          <img src="https://daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.jpg" class="w-full" />
+        </div>
+      </div>
+      <div class="flex justify-center w-full py-2 gap-2">
+        <a href="#item1" class="btn btn-xs">1</a>
+        <a href="#item2" class="btn btn-xs">2</a>
+        <a href="#item3" class="btn btn-xs">3</a>
+        <a href="#item4" class="btn btn-xs">4</a>
+      </div>
+
     </div>
+    <!-- notebook / gaming -->
+    <div class="notebookGaming">
+      <h1>notebookGaming</h1>
+    </div>
+    <!-- IT -->
+    <div class="it">
+      <h1>it</h1>
+    </div>
+    <!-- side -->
+    <div class="side">
+      <h1>side</h1>
+    </div>
+    <!-- อุปกรณ์เสริม -->
+    <div class="accessories">
+      <h1>accessories</h1>
+    </div>
+    <!-- provider Google -->
+    <div class="provider">
+      <h1>provider</h1>
+    </div>
+  </div>
 </template>
 
 <script setup>
 
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
